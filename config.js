55c54ed7bb835edb07df9988b88c7acde1742c98
drseu55 <alpics1337@gmail.com>
2022-08-02 11:28:43 +0300

    chore: Deploy new contract and ignore artifacts folder

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const AVALANCHE_TESTNET = "0xa521D9889DfC411CDf78f58248ee90c1eEf01b13";
 const AVALANCHE_TESTNET_API = "https://api.avax-test.network/ext/bc/C/rpc";
-const POLYGON_TESTNET = "0xF1047EC0EB28334A4858e4D5463B70d338Ea1940";
+const POLYGON_TESTNET = "0xb17F54e7d65d9665EBaAaC678307947cae706B76";
 const POLYGON_TESTNET_API = "https://rpc-mumbai.maticvigil.com/";
 
 export {
